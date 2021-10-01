9c895424cac3d1878e5e943ef6a98f861d3e0894
sujan100000 <dsilakar@gmail.com>
2021-10-01 18:58:20 +0545

    Promise assignment

--- promise.js
+++ promise.js
@@ -35,31 +35,31 @@ const getApiV1 = () => {
   
   // Normal Promise 
 
-  // getApiV1().
-  // then((result)=> {
-  //   const {data} = result ;
-  //   const realJson= JSON.parse (data);
-  //   for (let d in realJson.data) {
-  //     console.log ("id:", realJson.data[d].id);
-  //     console.log ("e-mail", realJson.data[d].email);
-  //     console.log ("First Name ", realJson.data[d].first_name);
-  //     console.log ("Last Name", realJson.data[d].last_name);
-  //     console.log ("Profile Picture ", realJson.data[d].avatar);
-  //   }
-  // }).catch((error) => {
-  //   console.log("Error occured during fetch operation")
-  // }).finally(() =>{
-  //   console.log ("Operation complete:");
-  // })
+  getApiV1().
+  then((result)=> {
+    const {data} = result ;
+    const realJson= JSON.parse (data);
+    for (let d in realJson.data) {
+      console.log ("id:", realJson.data[d].id);
+      console.log ("e-mail", realJson.data[d].email);
+      console.log ("First Name ", realJson.data[d].first_name);
+      console.log ("Last Name", realJson.data[d].last_name);
+      console.log ("Profile Picture ", realJson.data[d].avatar);
+    }
+  }).catch((error) => {
+    console.log("Error occured during fetch operation")
+  }).finally(() =>{
+    console.log ("Operation complete:");
+  })
 
 
   //   Promise.all 
    Promise.all([getApiV1(), getApiV2()])
-  // .then((result) => console.log("data:",result))
-  // .catch((err) => console.log("error:",err));
+  .then((result) => console.log("data:",result))
+  .catch((err) => console.log("error:",err));
 
-//   //   Promise.race 
-//     Promise.race([getApiV1(), getApiV2()])
-//     .then((result) => console.log("data:",result))
-//     .catch((err) => console.log("error:",err));
+ //   Promise.race 
+    Promise.race([getApiV1(), getApiV2()])
+    .then((result) => console.log("data:",result))
+    .catch((err) => console.log("error:",err));
     
